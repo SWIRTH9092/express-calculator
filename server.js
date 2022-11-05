4c47bc8478c52719fcd33b0dc611d8c0a42b886d
Your Name <you@example.com>
2022-11-05 12:02:52 -0400

    updated port to use EMV

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 const app = express()
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.get('/calc/:num1/:num2', (request,response) => {
     console.log("you are in get calc")
